dda005b7133526db25fbd0ceb00f8ac623e93e72
Abdul Wahab <148485953+AbdulWahab740@users.noreply.github.com>
2025-08-26 04:48:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 from utils.qna import build_chains
 
-st.title("Tech Ups Ai")
+st.title("Newsify")
 st.markdown(
     """
-    **Welcome to Tech Ups AI**
-    An application that allows you to ask questions about recent tech news.
-    The app uses a vector store to retrieve relevant articles and answer your questions.
+    **Welcome to Newsify!**
+    A application that allows you to ask questions about recent news.
+    The app uses a vector store to retrieve relevant scraped articles and answer your questions.
     """
 )
-st.subheader("Ask a question about any recent tech news:")
+st.subheader("Ask a question about any recent news: ")
 question = st.text_input("Enter your question here:")
 if question:
-    st.write(f"You asked: {question}")
+    st.markdown(f"You asked: {question}")
     response = build_chains(question)
     st.write(f"{response}")
 
