a081aa8530b114f92efc27c99c14d6e4e15a0b07
Abdul Wahab <148485953+AbdulWahab740@users.noreply.github.com>
2025-08-29 08:05:42 -0700

    Update scrape.py

--- utils/scrape.py
+++ utils/scrape.py
@@ -3,10 +3,13 @@ import streamlit as st
 
 APIKEY = st.secrets["APIKEY"]
 def get_news(query):
-    if (len(query.split(" "))> 1):
-        query = "+".join(query.split(" "))
+    # query = "Quantum Computing"
+    words = query.split()
+    # Join with single '+' between words
+    query = '+'.join(words)
     print(query)
     response = requests.get(f"https://gnews.io/api/v4/search?q={query}&lang=en&max=5&apikey={APIKEY}")
-    response.raise_for_status()
+    response.raise_for_status() 
     data = response.json()
     return data
+ 
